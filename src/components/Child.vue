693e1abe61f39cb675cb4e65e3ded3d344be30bc
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2020-01-13 00:48:13 +0100

    sample vue app

---
+++ src/components/Child.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="item">
+    <h2>{{ item }}</h2>
+    <img :src="url" width="235" height="300" />
+    <div class="quantity">
+      <button class="inc" @click="counter > 0 ? counter -= 1 : 0">-</button>
+      <span class="quant-text">Quantity: {{ counter }}</span>
+      <button class="inc" @click="counter += 1">+</button>
+    </div>
+    <button class="submit">Submit</button>
+  </div>
+  <!--item-->
+</template>
+
+<script>
+export default {
+  name: 'Child',
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  props: ['item', 'url'],
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
